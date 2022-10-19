78e5be0a8b070fcaa4b59dbc1be2c9f6027c97ab
Flipper <filipe@motionfirestudios.com.au>
2022-10-19 23:26:05 +0800

    added CSS styling for log in, sign up and home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import Highscores from '../components/Highscores';
 import css from './Home.module.css';
+import auth from '../utils/auth';
 
 const Home = () => {
   return (
     <div className={css.interface}>
-      <div className="my-4 text-center">
+      <div className={css.homeHeader}>
         <h2 className={css.logoHeader}>HOWZAT!</h2>
       </div>
       <div className="h-80 text-center py-4 px-5">
-        <div >
-          <h4 className={css.logoHeader}> Highscores </h4>
-        </div>
+        <h4 className={css.header}> Highscores </h4>
         <Highscores />
         <div className='row justify-center'>
-          <div className='p-2'>
-            <Link className="btn btn-primary btn-block mb-2" to="/login">
-              Login
-            </Link>
-          </div>
-          <div className='p-2'>
-            <Link className="btn btn-primary btn-block mb-2" to="/signup">
-              Signup
+          <Link className={css.interactiveButton} to="/login">
+            Login
+          </Link>
+          <div className={css.formReturn}>
+            <p className='mr-1 my-0'>New to the game?</p>
+            <Link to="/signup" className={css.linkText}>
+              Signup now
             </Link>
           </div>
         </div>
